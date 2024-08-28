c3a7ef8845b11cc210f5b6f85701a7455228236c
Vitor <contatovitordevasconcelos@gmail.com>
2024-08-28 09:50:36 -0300

    batman commit

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,29 @@
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl">Bem-vindo de volta!</h1>
+          <p className="text-base">
+            Faça seu login ou crie sua conta para acessar o easyBar!
+          </p>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignIn path="/sign-in" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animated-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className="h-full bg-[#5c0c44] hidden lg:flex items-center justify-center">
+        <Image src="/logo.svg" height={100} width={100} alt="Logo" />
+      </div>
+    </div>
+  );
+}
